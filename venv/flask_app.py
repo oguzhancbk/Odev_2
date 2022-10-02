d015352e3794b6ec4479ff6af11314ff7d76fcbe
Oguzhan Cabuk <oguzhan.cabuk.000@gmail.com>
2022-10-02 13:12:50 +0300

    venv

---
+++ venv/flask_app.py
@@ -0,0 +1,72 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+import pandas as pd
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Users(Resource):
+    def get(self):
+        data = pd.read_csv('users.csv')
+        data = data.to_dict('records')
+        return {'data': data}, 200
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', required=True , location= 'args')
+        parser.add_argument('surname', required=True, location= 'args')
+        parser.add_argument('age', required=True, location= 'args')
+        parser.add_argument('city', required=True, location= 'args')
+        args = parser.parse_args()
+
+        data = pd.read_csv('users.csv')
+
+        new_data = pd.DataFrame({
+            'name'      : [args['name']],
+            'surname': [args['surname']],
+            'age'       : [args['age']],
+            'city'      : [args['city']]
+        })
+
+        data = data.append(new_data, ignore_index = True)
+        data.to_csv('users.csv', index=False)
+        return {'data' : new_data.to_dict('records')}, 201
+
+    def delete(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', required=True, location= 'args')
+        args = parser.parse_args()
+
+        data = pd.read_csv('users.csv')
+
+        data = data[data['name'] != args['name']]
+
+        data.to_csv('users.csv', index=False)
+        return {'message': 'Record deleted successfully.'}, 200
+
+
+class Cities(Resource):
+    def get(self):
+        data = pd.read_csv('users.csv', usecols=[3])
+        data = data.to_dict('records')
+        return {'data': data}, 200
+
+
+class Name(Resource):
+    def get(self, name):
+        data = pd.read_csv('users.csv')
+        data = data.to_dict('records')
+        for entry in data:
+            if entry['name'] == name:
+                return {'data': entry}, 200
+        return {'message': 'No entry found with this name !'}, 200
+
+
+# Add URL endpoints
+api.add_resource(Users, '/users')
+api.add_resource(Cities, '/cities')
+api.add_resource(Name, '/<string:name>')
+
+if __name__ == '__main__':
+    app.run()
